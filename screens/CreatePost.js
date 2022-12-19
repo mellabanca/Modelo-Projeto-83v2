5ea39e55ef8ad4d8821ae0d1dc7bff6973472cfe
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-12-19 18:06:39 -0300

    Add files via upload

---
+++ screens/CreatePost.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    Image,
+    ScrollView,
+    TextInput
+} from "react-native";
+
+import { RFValue } from "react-native-responsive-fontsize";
+import DropDownPicker from "react-native-dropdown-picker";
+
+export default class CreatePost extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            previewImage: "image_1",
+            dropdownHeight: 40
+        };
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        let preview_images = {
+            image_1: require("../assets/image_1.jpg"),
+            image_1: require("../assets/image_1.jpg"),
+            image_2: require("../assets/image_2.jpg"),
+            image_3: require("../assets/image_3.jpg"),
+            image_4: require("../assets/image_4.jpg"),
+            image_5: require("../assets/image_5.jpg"),
+            image_6: require("../assets/image_6.jpg"),
+            image_7: require("../assets/image_7.jpg")
+        };
+        return (
+            //Adicione o código da dica 1 aqui
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "black"
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+    },
+    appTitle: {
+        flex: 0.07,
+        flexDirection: "row"
+    },
+    appIcon: {
+        flex: 0.3,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    iconImage: {
+        width: "100%",
+        height: "100%",
+        resizeMode: "contain"
+    },
+    appTitleTextContainer: {
+        flex: 0.7,
+        justifyContent: "center"
+    },
+    appTitleText: {
+        color: "white",
+        fontSize: RFValue(28)
+    },
+    fieldsContainer: {
+        flex: 0.85
+    },
+    previewImage: {
+        width: "93%",
+        height: RFValue(250),
+        alignSelf: "center",
+        borderRadius: RFValue(10),
+        marginVertical: RFValue(10),
+        resizeMode: "contain"
+    },
+    inputFont: {
+        height: RFValue(40),
+        borderColor: "white",
+        borderWidth: RFValue(1),
+        borderRadius: RFValue(10),
+        paddingLeft: RFValue(10),
+        color: "white"
+    }
+});
